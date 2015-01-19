4509c13e6b170f630de441fee34b03db170fc3a0
me0wth66 <quisinepeccatoest@gmail.com>
2015-01-19 10:29:15 -0600

    Initial commit

---
+++ application/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by me0wth66
+ * Date: 1/15/2015
+ * Time: 09:24 PM
+ */
+//Settings
+
+$db_host = ""; // Host
+$db_name = ""; // Database name
+$db_user = ""; // User name to access database
+$db_password = ""; // Password to access database
+
+// Do not change!
+$file_dir = "_posts"; // Directory to generate files
+
